a3f177376dce925564f034f4701f9f787e87a893
broken_Arm <brunotoro1799@gmail.com>
2023-11-12 19:04:39 -0300

    asd

---
+++ Recursividad/torres_hanoi.cpp
@@ -0,0 +1,29 @@
+/*
+PROBLEMA DE RECURSIVIDAD - TORRES DE HANOI
+
+Palo1       PALO2       PALO3
+  1           |           |
+  2           |           |
+  3           |           |
+  4           |           |
+
+Palo1= origen
+palo2= auxiliar
+palo3= destino
+
+reglas: mover de a una, la más chica siempre arriba
+
+Paso 1: Definir el caso base (solución inmediata)
+- caso base: si se tiene 1 solo disco, basta con desplazarlo al destino final
+
+
+*/
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int hanoi(n: int, o,d,a: STACK);
+
+int main(){
+
+}
