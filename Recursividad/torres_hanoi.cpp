1665e96ba54944604391cc93d37e777b341610d4
broken_Arm <brunotoro1799@gmail.com>
2023-11-12 20:34:13 -0300

    asd

--- Recursividad/torres_hanoi.cpp
+++ Recursividad/torres_hanoi.cpp
@@ -22,8 +22,24 @@ Paso 1: Definir el caso base (solución inmediata)
 #include <stack>
 using namespace std;
 
-int hanoi(n: int, o,d,a: STACK);
+int hanoi(int n,char origen,char auxiliar,char destino);
 
 int main(){
+    int elementos,movimiento;
+    cout<<"Ingresa cantidad discos: ";
+    cin>>elementos;
+    movimiento= hanoi(elementos,"Origen","Aux","C");
+    cout<<"Numero de movimientos: "<<movimiento<<endl;
+};
 
+int hanoi(int n,char a,char b,char c){
+    int movimiento=0;
+
+    if(elementos>0){
+        hanoi(n-1,a,b,c);
+        cout<<"se ha movido el disco "<<n<<" del "<<a<<" hasta la "<<c;
+        movimiento++;
+        hanoi(n-1,b,a,c)
+    }
+    return movimiento;
 }
