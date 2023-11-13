c6f3f102a989098546e69d6b59faca97d15bdca5
broken_Arm <brunotoro1799@gmail.com>
2023-11-12 23:21:10 -0300

    asd

--- Recursividad/torres_hanoi.cpp
+++ Recursividad/torres_hanoi.cpp
@@ -15,38 +15,33 @@ reglas: mover de a una, la más chica siempre arriba
 
 Paso 1: Definir el caso base (solución inmediata)
 - caso base: si se tiene 1 solo disco, basta con desplazarlo al destino final
-
-
 */
 #include <iostream>
 using namespace std;
 
-int hanoi(int,String,String,String);
+int hanoi(int,string,string,string);
 
 int main()
 {
     int elementos,movimiento;
     cout<<"Ingresa cantidad discos: ";
     cin>>elementos;
-    movimiento= hanoi(elementos,'ORIGEN','AUX','DESTINO');
+    movimiento= hanoi(elementos,"ORIGEN","AUX","DESTINO");
     cout<<"Numero de movimientos: "<<movimiento<<endl;
     return 0;
 };
 
-int hanoi(int n,String inicio,String aux,String destino){
-    int movimientos=0;
+int hanoi(int n, string inicio, string aux, string destino) {
+    int movimientos = 0;
 
-    if(n==1){
-        cout<<"Mueve el disco "<<n<<" del "<<inicio<<" al "<<destino<<endl;
+    if (n == 1) {
+        cout << "Mueve el disco " << n << " del " << inicio << " al " << destino << endl;
         movimientos++;
-    }else{
-        hanoi(n-1,inicio,destino,aux);
-        cout<<"Mueve el disco "<<n<<" del "<<inicio<<" al "<<destino;
+    } else {
+        movimientos += hanoi(n - 1, inicio, destino, aux); // Movimiento antes de imprimir
+        cout << "Mueve el disco " << n << " del " << inicio << " al " << destino << endl;
         movimientos++;
-        hanoi(n-1,aux,inicio,destino);
+        movimientos += hanoi(n - 1, aux, inicio, destino); // Movimiento después de imprimir
     }
-
-
-
     return movimientos;
 }
