c8724d4412640737afa6043f0c778c59c602cac5
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2024-01-08 17:23:49 -0300

    asd

---
+++ CursoVerano/matricesPunteros.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m[5][10];
+    int*h= &m[0][0];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<10;j++){
+            if(i%2 && j%2){
+                *(h+i)=i+j;
+            }
+            if(!i%2 && j%2){
+                *(h+j)=i+j;
+            }else{
+                *(h+(i*j))= *h+(i*j);
+            }
+        }
+    }
+    
+    for(int i=0;i<5;i++){
+        for(int j=0;j<10;j++){
+            cout<<m[i][j]<<", ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*
+RESULTADO:
+
+0, 10, 2, 12, 4, 5, 6, 7, 8, 9, 
+10, 0, 12, 0, 14, 15, 16, 0, 18, 0, 
+20, 21, 0, 0, 24, 0, 0, 27, 28, 0, 
+0, 0, 32, 0, 0, 0, 36, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+
+PARA INSTANCIAR UN VECTOR DE PUNTEROS, SE HACE ASÍ
+int* listaPunteros[5]
+*/
